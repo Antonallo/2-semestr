0a52d520bbb68ba31ff37303398cdb6143aaa14e
Antonallo <100342766+Antonallo@users.noreply.github.com>
2023-06-10 00:33:31 +0200

    task_2

---
+++ task_2/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+void my_memcpy(void* dst, const void* src, size_t size) {
+    char* dst_bytes = (char*)dst;
+    const char* src_bytes = (const char*)src;
+    for (size_t i = 0; i < size; ++i)
+        dst_bytes[i] = src_bytes[i];
+}
+
+int main() {
+    char str1[] = "Hello, world!";
+    char str2[20];
+    my_memcpy(str2, str1, strlen(str1) + 1);
+    std::cout << str2;
+    return 0;
+}
+        
